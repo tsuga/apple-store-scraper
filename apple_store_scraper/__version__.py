69e6b9f8cd8e7b51f47a4448a81d231aeb14cd14
tsuga <2888173+tsuga@users.noreply.github.com>
2024-08-22 15:54:41 +0400

    Update __version__.py

--- apple_store_scraper/__version__.py
+++ apple_store_scraper/__version__.py
@@ -1,5 +1,5 @@
 __title__ = "apple-store-scraper"
-__version__ = "0.3.6"
+__version__ = "0.3.6b"
 __description__ = "Single API ☝ App Store Review Scraper 🧹"
 __author__ = "Eric Lim"
 __url__ = "https://github.com/hiyali/apple-store-scraper"
